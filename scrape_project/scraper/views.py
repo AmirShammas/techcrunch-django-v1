0fdb1809a9de9c50a933095fd0d6ed08834309ab
ShammasAmir <amirshammas@gmail.com>
2024-04-29 15:45:35 +0330

    Add views.

--- scrape_project/scraper/views.py
+++ scrape_project/scraper/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import SearchForm
+from .tasks import search_task
 
-# Create your views here.
+
+def SearchView(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            result = search_task.delay(
+                keyword=form.cleaned_data['keyword'],
+                page_count=form.cleaned_data['page_count'],
+            )
+            print('search_task results:', result)
+    else:
+        form = SearchForm()
+
+    return render(request, 'templates/search.html', {'form': form})
+
